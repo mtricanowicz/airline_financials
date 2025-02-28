6d9198c40b8f80812ab0712ab06a7304442a2662
mtricanowicz <michael.tricanowicz@live.com>
2025-02-28 12:06:50 -0500

    minor update to metric definitions

--- airline_comparison.py
+++ airline_comparison.py
@@ -144,7 +144,7 @@ metric_definitions = [
     ("Net Income", "Profit."),
     ("Revenue Passenger Mile (RPM)", "A basic measure of sales volume. One RPM represents one passenger flown one mile."),
     ("Available Seat Mile (ASM)", "A basic measure of production. One ASM represents one seat flown one mile."),
-    ("Long-Term Debt", "Total long-term debt net of current maturities.<br>NOTE: Due to inconsistent reporting in quarterly filings between airilnes, this metric is only shown for full year data and sourced from 10K filings."),
+    ("Long-Term Debt", "Total long-term debt net of current maturities.<br>NOTE: This metric is reported annually and is therefore shown only for full year data and sourced from 10K filings."),
     ("Profit Sharing", "Amount of income set aside to fund employee profit sharing programs.<br>NOTE: Quarterly reporting by AAL and UAL of this metric is inconsistent. Data provided may have been obtained from internal sources or estimated by proportioning the annual profit sharing reported by the quarterly operating income reported."),
     ("Operating Margin", "Operating Income divided by Total Revenue"),
     ("Net Margin", "Percentage of profit earned for each dollar in revenue. Net Income divided by Total Revenue."),
